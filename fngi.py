43483335158ce6598eaf29005b8d3445fb629955
Rett Berg <vitiral@gmail.com>
2021-06-17 22:36:37 -0600

    move Environment and dependencies

--- fngi.py
+++ fngi.py
@@ -10,150 +10,6 @@ from typing import List
 from typing import Tuple
 from typing import Dict
 
-# Most of our code is going to have some basic tests inline.  Tests can be run
-# by installing pytest and running it.
-
-def needAlign(size: int) -> int:
-    """Return padding bytes needed to align a value of size."""
-    if size % 4 == 0:
-        return 0
-    return 4 - (size % 4)
-
-def testNeedAlign():
-    assert 0 == needAlign(0)
-    assert 3 == needAlign(1)
-    assert 2 == needAlign(2)
-    assert 1 == needAlign(3)
-    assert 0 == needAlign(4)
-
-# Types in the compiler are represented by a class. We sometimes use
-# uninstantiated classes to represent types, mostly because for
-# structs that is the only way python permits using ctypes.Structure subclasses.
-# For other types we typically use instantiated classes.
-#
-# Types are registered with the global TYS dictionary for lookup during
-# compilation.  For the stage0 fngi compiler we are building here, there are no
-# namespaces or modules so we don't have to worry about that complexity.
-
-class Ty(object):
-    """The base type that all fngi types derive from.
-    """
-    def __init__(self):
-        super().__init__()
-
-    def name(self):
-        return self.__class__.__name__
-
-TYS: Dict[str, Ty] = {} # Global registry
-
-
-class DataTy(ctypes.Structure, Ty):
-    """A type for data represented in memory.
-
-    Reading/Writing a DataType (dt) to a bytearray (ba):
-    - reading => dt = MyDataType.from_buffer(ba)
-    - writing => ba[start:start+dt.size()] = dt
-
-    Python's ctypes.Structure class allows us to get C-compliant structures
-    "out of the box" with minimal effort.
-    """
-    @classmethod
-    def size(cls):
-        return ctypes.sizeof(cls)
-
-    @classmethod
-    def fieldOffset(cls, field: str):
-        return getattr(cls, field).offset
-
-    @classmethod
-    def fieldSize(cls, field: str):
-        return getattr(cls, field).size
-
-# Fungi uses 32 bits for its pointers.
-ctype_ptr = ctypes.c_uint32
-
-def createCoreTy(name, cty):
-    """Creates a core data type and registers it.
-
-    This is soley to reduce boilerplace. It is the same as:
-
-        class {{name}}(DataTy): _fields_ =  [('v', {{cty}})]
-        TYS["{{name}}"] = {{name}}
-    """
-    nativeTy = type(name, tuple([DataTy]), {'_fields_': [('v', cty)]})
-    TYS[name] = nativeTy
-    return nativeTy
-
-
-I8 = createCoreTy("I8",  ctypes.c_int8)
-U8 = createCoreTy("U8",  ctypes.c_uint8)
-I16 = createCoreTy("I16", ctypes.c_int16)
-U16 = createCoreTy("U16", ctypes.c_uint16)
-I32 = createCoreTy("I32", ctypes.c_int32)
-U32 = createCoreTy("U32", ctypes.c_uint32)
-I64 = createCoreTy("I64", ctypes.c_int64)
-U64 = createCoreTy("U64", ctypes.c_uint64)
-Ptr = createCoreTy("Ptr", ctype_ptr) # an address in memory, no type
-
-class Ref(DataTy):
-    """The Ref type has a "generic" refTy it is associated with."""
-    _fields_ = [('v', ctype_ptr)]
-
-    def __init__(self, v: int, refTy: Ty):
-        self.refTy = refTy
-        # Note: reftypes are not registered ty TYS
-
-class Fn(Ty):
-    """The base Fn type.
-
-    New function types are created by instantiating a subclass of Fn.
-    """
-
-    def __init__(self, name: str, inputs: List[Ty], outputs: List[Ty]):
-        super().__init__()
-        self.name, self.inputs, self.outputs = name, inputs, outputs
-
-    @abc.abstractmethod
-    def call(self):
-        raise TypeError("Called call on a non-native fn")
-
-    @abc.abstractmethod
-    def fnIndex(self):
-        raise TypeError("Called fnIndex on a non-user fn")
-
-
-class NativeFn(Fn):
-    """A native function.
-
-    Aka a function implemented in python which modifies the DATA_STACK.
-    """
-    def __init__(self, name: str, inputs: List[Ty], outputs: List[Ty], call: Callable[[], None]):
-        super().__init__(name, inputs, outputs)
-        self._call = call
-
-    def fnIndex(self):
-        raise TypeError("Called fnIndex on a non-user fn")
-
-    def call(self):
-        self._call()
-
-
-class UserFn(Fn):
-    """A user-created function defined in fngi source code.
-
-    Functions are compiled to the CALL_SPACE and therefore have an index.
-    """
-    def __init__(self, name: str, inputs: List[Ty], outputs: List[Ty], fnIndex: int):
-        super().__init__(name, inputs, outputs)
-        self._fnIndex = fnIndex
-
-        # Handle registration
-        TYS[name] = self
-        FN_INDEX_LOOKUP[fnIndex] = self
-
-    def fnIndex(self):
-        return self._fnIndex
-
 # Most data in fngi is passed on either the DATA_STACK, RET_STACK or HEAP. LOCALS
 # are kept inside of the RET_STACK.
 #
@@ -162,7 +18,6 @@ class UserFn(Fn):
 #
 # The other data regions (RET_STACK, HEAP, TYPE_STACK, etc) are all slices of the
 # global MEMORY region.
-
 
 class Heap(object):
     def __init__(self, memory):
@@ -238,30 +93,56 @@ class Stack(object):
     def __repr__(self):
         return "STACK<{}/{}>".format(len(self), self.total_size)
 
-
-##############################################################################
-# The Global Environment and Execution Loop
+# Types in the compiler are represented by a class. We sometimes use
+# uninstantiated classes to represent types, mostly because for
+# structs that is the only way python permits using ctypes.Structure subclasses.
+# For other types we typically use instantiated classes.
 #
-# These are presented first so that it is clear how simple things are. Experienced
-# programmers may notice the similarities to the execution model of Forth, which
-# is intentional, because fngi will eventually be implemented in Forth. This (python)
-# implementation is for prototyping and to provide a reference/learning manual for the
-# stage0 fngi language. It will be kept up-to-date with the stage0 forth compiler.
-#
-# For the execution model, there are two types of Fn (function), NativeFn which
-# are implemented in python and UserFn which are simply a range of indexes
-# inside of the CALL_SPACE global variable; which are run by callLoop.
-#
-# Although functions have types (i.e. inputs/outputs), the types are only
-# checked at compile time. At execution time, functions pop values off of the
-# DATA_STACK for their parameters and push values on the DATA_STACK for their
-# results. They also use the RET_STACK and BSP for keeping track of local
-# variables.
+# Types are registered with the global TYS dictionary for lookup during
+# compilation.  For the stage0 fngi compiler we are building here, there are no
+# namespaces or modules so we don't have to worry about that complexity.
 
-# Global Environment
-RUNNING = False # must stay true for callLoop to keep running.
+class Ty(object):
+    """The base type that all fngi types derive from.
+    """
+    def __init__(self):
+        super().__init__()
 
-MiB = 2**20 # 1 Mebibyte
+    def name(self):
+        return self.__class__.__name__
+
+class Fn(Ty):
+    """The base Fn type.
+
+    New function types are created by instantiating a subclass of Fn.
+    """
+
+    def __init__(self, name: str, inputs: List[Ty], outputs: List[Ty]):
+        super().__init__()
+        self.name, self.inputs, self.outputs = name, inputs, outputs
+
+    @abc.abstractmethod
+    def call(self):
+        raise TypeError("Called call on a non-native fn")
+
+    @abc.abstractmethod
+    def fnIndex(self):
+        raise TypeError("Called fnIndex on a non-user fn")
+
+
+# TODO: explain
+class Environment(object):
+    def __init__(
+            self,
+            callSpace: List[Fn],
+            memory: bytearray,
+            dataStack: Stack,
+            retStack: Stack):
+        self.callSpace, self.memory = callSpace, memory
+        self.dataStack, self.retStack = dataStack, retStack
+        self.running = False
+        self.fnIndex = 0
+
 
 MEMORY = bytearray(50 * MiB) # 50 MiB Global Memory
 HEAP = Heap(MEMORY)
@@ -283,19 +164,163 @@ TYPE_STACK = Stack(MEMORY, HEAP.grow(STACK_SIZE), STACK_SIZE)
 DEFER_STACK = Stack(MEMORY, HEAP.grow(STACK_SIZE), STACK_SIZE)
 LOCALS: DataTy = None
 
-def callLoop():
+GLOBAL_ENVIRONMENT = Environemnt(CALL_SPACE, MEMORY
+
+# Most of our code is going to have some basic tests inline.  Tests can be run
+# by installing pytest and running it.
+
+def needAlign(size: int) -> int:
+    """Return padding bytes needed to align a value of size."""
+    if size % 4 == 0:
+        return 0
+    return 4 - (size % 4)
+
+def testNeedAlign():
+    assert 0 == needAlign(0)
+    assert 3 == needAlign(1)
+    assert 2 == needAlign(2)
+    assert 1 == needAlign(3)
+    assert 0 == needAlign(4)
+
+
+TYS: Dict[str, Ty] = {} # Global registry
+
+
+class DataTy(ctypes.Structure, Ty):
+    """A type for data represented in memory.
+
+    Reading/Writing a DataType (dt) to a bytearray (ba):
+    - reading => dt = MyDataType.from_buffer(ba)
+    - writing => ba[start:start+dt.size()] = dt
+
+    Python's ctypes.Structure class allows us to get C-compliant structures
+    "out of the box" with minimal effort.
+    """
+    @classmethod
+    def size(cls):
+        return ctypes.sizeof(cls)
+
+    @classmethod
+    def fieldOffset(cls, field: str):
+        return getattr(cls, field).offset
+
+    @classmethod
+    def fieldSize(cls, field: str):
+        return getattr(cls, field).size
+
+# Fungi uses 32 bits for its pointers.
+ctype_ptr = ctypes.c_uint32
+
+def createCoreTy(name, cty):
+    """Creates a core data type and registers it.
+
+    This is soley to reduce boilerplace. It is the same as:
+
+        class {{name}}(DataTy): _fields_ =  [('v', {{cty}})]
+        TYS["{{name}}"] = {{name}}
+    """
+    nativeTy = type(name, tuple([DataTy]), {'_fields_': [('v', cty)]})
+    TYS[name] = nativeTy
+    return nativeTy
+
+
+I8 = createCoreTy("I8",  ctypes.c_int8)
+U8 = createCoreTy("U8",  ctypes.c_uint8)
+I16 = createCoreTy("I16", ctypes.c_int16)
+U16 = createCoreTy("U16", ctypes.c_uint16)
+I32 = createCoreTy("I32", ctypes.c_int32)
+U32 = createCoreTy("U32", ctypes.c_uint32)
+I64 = createCoreTy("I64", ctypes.c_int64)
+U64 = createCoreTy("U64", ctypes.c_uint64)
+Ptr = createCoreTy("Ptr", ctype_ptr) # an address in memory, no type
+
+class Ref(DataTy):
+    """The Ref type has a "generic" refTy it is associated with."""
+    _fields_ = [('v', ctype_ptr)]
+
+    def __init__(self, v: int, refTy: Ty):
+        self.refTy = refTy
+        # Note: reftypes are not registered ty TYS
+
+
+
+class NativeFn(Fn):
+    """A native function.
+
+    Aka a function implemented in python which modifies the DATA_STACK.
+    """
+    def __init__(
+            self,
+            name: str,
+            inputs: List[Ty],
+            outputs: List[Ty],
+            call: Callable[[Environment], None]):
+        super().__init__(name, inputs, outputs)
+        self._call = call
+
+    def fnIndex(self):
+        raise TypeError("Called fnIndex on a non-user fn")
+
+    def call(self, env: Environment):
+        self._call(env)
+
+
+class UserFn(Fn):
+    """A user-created function defined in fngi source code.
+
+    Functions are compiled to the CALL_SPACE and therefore have an index.
+    """
+    def __init__(self, name: str, inputs: List[Ty], outputs: List[Ty], fnIndex: int):
+        super().__init__(name, inputs, outputs)
+        self._fnIndex = fnIndex
+
+        # Handle registration
+        TYS[name] = self
+        FN_INDEX_LOOKUP[fnIndex] = self
+
+    def fnIndex(self):
+        return self._fnIndex
+
+
+##############################################################################
+# The Global Environment and Execution Loop
+#
+# These are presented first so that it is clear how simple things are. Experienced
+# programmers may notice the similarities to the execution model of Forth, which
+# is intentional, because fngi will eventually be implemented in Forth. This (python)
+# implementation is for prototyping and to provide a reference/learning manual for the
+# stage0 fngi language. It will be kept up-to-date with the stage0 forth compiler.
+#
+# For the execution model, there are two types of Fn (function), NativeFn which
+# are implemented in python and UserFn which are simply a range of indexes
+# inside of the CALL_SPACE global variable; which are run by callLoop.
+#
+# Although functions have types (i.e. inputs/outputs), the types are only
+# checked at compile time. At execution time, functions pop values off of the
+# DATA_STACK for their parameters and push values on the DATA_STACK for their
+# results. They also use the RET_STACK and BSP for keeping track of local
+# variables.
+
+
+def callLoop(env: Environment):
     """Yup, this is the entire call loop."""
-    while RUNNING:
-        fn = CALL_SPACE[FN_INDEX]
+    while env.running:
+        fn = env.callSpace[env.fnIndex]
         if isinstance(fn, NativeFn):
             # If native, run the function
-            FN_INDEX += 1
-            fn.call()
+            env.fnIndex += 1
+            fn.call(env)
         else:
             # If not native, store the next index on the call stack
-            callStack.pushValue(u32, FN_INDEX + 1)
+            callStack.pushValue(u32, env.fnIndex + 1)
             # and "run" the function by changing the FN_INDEX to be it
-            FN_INDEX = fn.fnIndex()
+            env.fnIndex = fn.fnIndex()
+
+# Global Environment
+RUNNING = False # must stay true for callLoop to keep running.
+
+MiB = 2**20 # 1 Mebibyte
+
 
 def retCall():
     FN_INDEX = RET_STACK[BSP - 4]
@@ -307,9 +332,9 @@ def addU32Call():
 addU32 = NativeFn("addU32", [U32, U32], [U32], addU32Call)
 
 
-class Constant(Fn):
+class Literal(Fn):
     def __init__(self, value: DataTy):
-        super().__init__(ty.name() + 'Constant', [], [value.__class__])
+        super().__init__(ty.name() + 'Literal', [], [value.__class__])
         self.value = value
 
     def call(self):
@@ -324,6 +349,10 @@ def nativeFn(inputs: List[Ty], outputs: List[Ty]):
         # Handle registration
         TYS[name] = nativeFn
     return wrapper
+
+
+def testBasicEnvironment():
+    pass
 
 # Parser
 
