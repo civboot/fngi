4143a96b4309b870a911291f5a5af9508345994a
Rett Berg <rett@google.com>
2021-06-24 00:32:08 -0600

    add bytecode spec docs.

--- fngi.py
+++ fngi.py
@@ -1,3 +1,13 @@
+# Python implementation of fngi language.
+#
+# This implementation simultaniously defines a "bytecode spec" for the stage0
+# compiler runtime. This will allow a future implementation (aka in Forth)
+# to have a reference for testing and inspecting the runtime, including
+# variables, function defintions, etc.
+#
+# This requires careful and extensive use of python's `ctypes` module,
+# which provides c-like (and therefore fngi-like) datatypes for us to use.
+
 import abc
 import sys
 import struct
