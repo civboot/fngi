d838cd560cf09e41c7af2956f27eaee075464f7b
Rett Berg <vitiral@gmail.com>
2021-07-05 08:11:13 -0600

    starting to test formal grammar

---
+++ test_grammar.py
@@ -0,0 +1,27 @@
+import re
+import parsimonious
+from parsimonious.grammar import Grammar
+
+def readf(path):
+    with open(path) as f:
+        return f.read()
+
+grammarTxt = readf('grammar/full_grammar.peg')
+
+RAW_REGEX = re.compile(r'"[^"]*"')
+
+def testRawString():
+    source = r'''"foo bar 'baz' \nbob"'''
+    assert source == RAW_REGEX.match(source).group(0)
+
+def getGrammar():
+    return Grammar(grammarTxt)
+
+def testFullGrammar():
+    grammar = getGrammar()
+
+def testParse_simple():
+    txt = readf('grammar/simple.fn')
+    grammar = getGrammar()
+    grammar.parse(txt)
+
