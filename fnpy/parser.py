38cf9daa2e356868be1b47618291fbadbfc88eed
Rett Berg <vitiral@gmail.com>
2021-07-13 17:12:21 -0600

    implement parseMultiLabelStmt

--- fnpy/parser.py
+++ fnpy/parser.py
@@ -8,12 +8,12 @@
 # parse and emit lexemes (sometimes also called tokens).
 
 import copy
-import dataclasses
 import enum
 import os
 import io
 import pprint
 
+from dataclasses import dataclass
 from typing import List
 
 ## Lexemes
@@ -92,73 +92,74 @@ class LexemeVariant(enum.Enum):
     RETURN = enum.auto()
 
 
+LV = LexemeVariant
 
 
-@dataclasses.dataclass
+@dataclass
 class Lexeme:
-    variant: LexemeVariant
+    variant: LV
     text: str = None
 
 
-EOF = Lexeme(LexemeVariant.EOF)
+EOF = Lexeme(LV.EOF)
 
-MACRO1 = Lexeme(LexemeVariant.MACRO1)
-MACRO2 = Lexeme(LexemeVariant.MACRO2)
-MINUS = Lexeme(LexemeVariant.MINUS)
-REF = Lexeme(LexemeVariant.REF)
-DEREF = Lexeme(LexemeVariant.DEREF)
-CALL = Lexeme(LexemeVariant.CALL)
-DIVIDE = Lexeme(LexemeVariant.DIVIDE)
-MULTIPLY = Lexeme(LexemeVariant.MULTIPLY)
-PLUS = Lexeme(LexemeVariant.PLUS)
-GE = Lexeme(LexemeVariant.GE)
-GT = Lexeme(LexemeVariant.GT)
-LE = Lexeme(LexemeVariant.LE)
-LT = Lexeme(LexemeVariant.LT)
-NE = Lexeme(LexemeVariant.NE)
-EQ = Lexeme(LexemeVariant.EQ)
-SET = Lexeme(LexemeVariant.SET)
+MACRO1 = Lexeme(LV.MACRO1)
+MACRO2 = Lexeme(LV.MACRO2)
+MINUS = Lexeme(LV.MINUS)
+REF = Lexeme(LV.REF)
+DEREF = Lexeme(LV.DEREF)
+CALL = Lexeme(LV.CALL)
+DIVIDE = Lexeme(LV.DIVIDE)
+MULTIPLY = Lexeme(LV.MULTIPLY)
+PLUS = Lexeme(LV.PLUS)
+GE = Lexeme(LV.GE)
+GT = Lexeme(LV.GT)
+LE = Lexeme(LV.LE)
+LT = Lexeme(LV.LT)
+NE = Lexeme(LV.NE)
+EQ = Lexeme(LV.EQ)
+SET = Lexeme(LV.SET)
 
-SEMICOLON = Lexeme(LexemeVariant.SEMICOLON)
-COLON = Lexeme(LexemeVariant.COLON)
-EQUAL = Lexeme(LexemeVariant.EQUAL)
-BLOCK_OPEN = Lexeme(LexemeVariant.BLOCK_OPEN)
-BLOCK_CLOSE = Lexeme(LexemeVariant.BLOCK_CLOSE)
-DATA_OPEN = Lexeme(LexemeVariant.DATA_OPEN)
-DATA_CLOSE = Lexeme(LexemeVariant.DATA_CLOSE)
-TYPE_OPEN = Lexeme(LexemeVariant.TYPE_OPEN)
-TYPE_CLOSE = Lexeme(LexemeVariant.TYPE_CLOSE)
+SEMICOLON = Lexeme(LV.SEMICOLON)
+COLON = Lexeme(LV.COLON)
+EQUAL = Lexeme(LV.EQUAL)
+BLOCK_OPEN = Lexeme(LV.BLOCK_OPEN)
+BLOCK_CLOSE = Lexeme(LV.BLOCK_CLOSE)
+DATA_OPEN = Lexeme(LV.DATA_OPEN)
+DATA_CLOSE = Lexeme(LV.DATA_CLOSE)
+TYPE_OPEN = Lexeme(LV.TYPE_OPEN)
+TYPE_CLOSE = Lexeme(LV.TYPE_CLOSE)
 
-ARROW = Lexeme(LexemeVariant.ARROW)
-DOT = Lexeme(LexemeVariant.DOT)
-LABEL = Lexeme(LexemeVariant.LABEL)
+ARROW = Lexeme(LV.ARROW)
+DOT = Lexeme(LV.DOT)
+LABEL = Lexeme(LV.LABEL)
 
-FN = Lexeme(LexemeVariant.FN)
-DO = Lexeme(LexemeVariant.DO)
-STRUCT = Lexeme(LexemeVariant.STRUCT)
-ENUM = Lexeme(LexemeVariant.ENUM)
-IMPL = Lexeme(LexemeVariant.IMPL)
-STK = Lexeme(LexemeVariant.STK)
-ARR = Lexeme(LexemeVariant.ARR)
+FN = Lexeme(LV.FN)
+DO = Lexeme(LV.DO)
+STRUCT = Lexeme(LV.STRUCT)
+ENUM = Lexeme(LV.ENUM)
+IMPL = Lexeme(LV.IMPL)
+STK = Lexeme(LV.STK)
+ARR = Lexeme(LV.ARR)
 
-IF = Lexeme(LexemeVariant.IF)
-ELIF = Lexeme(LexemeVariant.ELIF)
-ELDO = Lexeme(LexemeVariant.ELDO)
-SWITCH = Lexeme(LexemeVariant.SWITCH)
-CASE = Lexeme(LexemeVariant.CASE)
-WHILE = Lexeme(LexemeVariant.WHILE)
+IF = Lexeme(LV.IF)
+ELIF = Lexeme(LV.ELIF)
+ELDO = Lexeme(LV.ELDO)
+SWITCH = Lexeme(LV.SWITCH)
+CASE = Lexeme(LV.CASE)
+WHILE = Lexeme(LV.WHILE)
 
-NOT = Lexeme(LexemeVariant.NOT)
-AND = Lexeme(LexemeVariant.AND)
-OR = Lexeme(LexemeVariant.OR)
-BITNOT = Lexeme(LexemeVariant.BITNOT)
-BITOR = Lexeme(LexemeVariant.BITOR)
-BITXOR = Lexeme(LexemeVariant.BITXOR)
-BITAND = Lexeme(LexemeVariant.BITAND)
+NOT = Lexeme(LV.NOT)
+AND = Lexeme(LV.AND)
+OR = Lexeme(LV.OR)
+BITNOT = Lexeme(LV.BITNOT)
+BITOR = Lexeme(LV.BITOR)
+BITXOR = Lexeme(LV.BITXOR)
+BITAND = Lexeme(LV.BITAND)
 
-CONT = Lexeme(LexemeVariant.CONT)
-BREAK = Lexeme(LexemeVariant.BREAK)
-RETURN = Lexeme(LexemeVariant.RETURN)
+CONT = Lexeme(LV.CONT)
+BREAK = Lexeme(LV.BREAK)
+RETURN = Lexeme(LV.RETURN)
 
 ################################
 # Scanner
@@ -200,12 +201,14 @@ class Scanner(object):
             self.bufMax = self.bufIndex + len(readBytes)
             self.buffer[self.bufIndex:self.bufMax] = readBytes
 
-        if self.bufIndex == self.bufMax:
-            return 0 # EOF
-
         self.lastCol = self.col
 
-        b = self.buffer[self.bufIndex]
+        if self.bufIndex == self.bufMax:
+            # We pretend we are returning a character even though it is the EOF
+            # so that backByte still works.
+            b = 0 # EOF
+        else:
+            b = self.buffer[self.bufIndex]
         self.bufIndex += 1
         self.filePos += 1
         if b == ord('\n'):
@@ -227,6 +230,22 @@ class Scanner(object):
         self.lastCol = self.LAST_COL_SENTINEL
         if self.buffer[self.bufIndex] == ord('\n'):
             self.line -= 1
+
+def testScanner_basic():
+    sc = Scanner(io.BytesIO(b'42'))
+    for i in range(10):
+        assert ord('4') == sc.nextByte()
+        sc.backByte()
+    assert ord('4') == sc.nextByte()
+    for i in range(10):
+        assert ord('2') == sc.nextByte()
+        sc.backByte()
+    assert ord('2') == sc.nextByte()
+    for i in range(10):
+        assert 0 == sc.nextByte()
+        sc.backByte()
+    assert 0 == sc.nextByte()
+
 
 ################################
 # Lexer
@@ -254,7 +273,7 @@ def isHex(c: int) -> bool:
         or (ord('a') <= c <= ord('f')))
 
 def isNameChar(c: int) -> bool:
-    return not isWhitespace(c) and not isSymbol(c)
+    return c != 0 and not isWhitespace(c) and not isSymbol(c)
 
 
 class Lexer(object):
@@ -263,7 +282,6 @@ class Lexer(object):
         self.scanner = Scanner(fo)
 
         self.fo = fo
-        self.fileSize = os.stat(fo.fileno()).st_size
         self.tokenIndex = 0
 
     def nextLexeme(self) -> Lexeme:
@@ -287,7 +305,7 @@ class Lexer(object):
                         break
                     out.append(c)
                     c = sc.nextByte()
-                return Lexeme(LexemeVariant.LINE_COMMENT, out)
+                return Lexeme(LV.LINE_COMMENT, out)
             elif c == ord('*'): # BLOCK_COMMENT
                 out = bytearray(b'/*')
                 lastWasStar = False
@@ -297,7 +315,7 @@ class Lexer(object):
                     if c == '*':
                         lastWasStar = True
                     elif c == 0 or (lastWasStar and c == '/'):
-                        return Lexeme(LexemeVariant.BLOCK_COMMENT, out)
+                        return Lexeme(LV.BLOCK_COMMENT, out)
                     else:
                         lastWasStar = False
 
@@ -309,12 +327,12 @@ class Lexer(object):
             c = sc.nextByte()
             while True:
                 if c == 0:
-                    return Lexeme(LexemeVariant.INVALID, out)
+                    return Lexeme(LV.INVALID, out)
                 out.append(c)
                 if c == ord('"'):
                     break
                 c = sc.nextByte()
-            return Lexeme(LexemeVariant.RAW_STR, out)
+            return Lexeme(LV.RAW_STR, out)
 
         elif c == ord('!'):
             c = sc.nextByte()
@@ -357,7 +375,7 @@ class Lexer(object):
             c = sc.nextByte()
             out.append(c)
             if c != ord('"'):
-                return Lexeme(LexemeVariant.INVALID, out)
+                return Lexeme(LV.INVALID, out)
 
             while True:
                 c = sc.nextByte()
@@ -368,7 +386,7 @@ class Lexer(object):
                     if c == ord('"'):
                         break
 
-            return Lexeme(LexemeVariant.ESC_STR, out)
+            return Lexeme(LV.ESC_STR, out)
 
         elif ord('0') <= c <= ord('9'):
             out = bytearray([c])
@@ -380,17 +398,19 @@ class Lexer(object):
                 out.append(c)
                 c = sc.nextByte()
 
-            if numIsHex and isHex(c):
-                out.append(c)
-            elif isNumber(c):
-                out.append(c)
-            elif isNameChar(c):
-                sc.backByte()
-                out.extend(b' followed by nameChar')
-                return Lexeme(LexemeVariant.INVALID, out)
-            else:
-                sc.backByte()
-                return Lexeme(LexemeVariant.NUMBER, out)
+            while True:
+                if numIsHex and isHex(c):
+                    out.append(c)
+                elif isNumber(c):
+                    out.append(c)
+                elif isNameChar(c):
+                    sc.backByte()
+                    out.extend(b' followed by nameChar')
+                    return Lexeme(LV.INVALID, out)
+                else:
+                    sc.backByte()
+                    return Lexeme(LV.NUMBER, out)
+                c = sc.nextByte()
 
         elif c == ord('&'): return REF
         elif c == ord('@'): return DEREF
@@ -408,7 +428,7 @@ class Lexer(object):
         elif c == ord('}'): return DATA_CLOSE
         elif c == ord('['): return TYPE_OPEN
         elif c == ord(']'): return TYPE_CLOSE
-        elif isSymbol(c): return Lexeme(LexemeVariant.INVALID, bytearray([c]))
+        elif isSymbol(c): return Lexeme(LV.INVALID, bytearray([c]))
 
         name = bytearray([c])
         while True:
@@ -444,7 +464,7 @@ class Lexer(object):
         if name == b'break': return BREAK
         if name == b'return': return RETURN
 
-        return Lexeme(LexemeVariant.IDEN, name)
+        return Lexeme(LV.IDEN, name)
 
     def getAllLexemes(self) -> List[Lexeme]:
         """Used in testing."""
@@ -455,14 +475,28 @@ class Lexer(object):
             if lexeme is EOF:
                 return out
 
-    def getAllVariants(self) -> List[LexemeVariant]:
+    def getAllVariants(self) -> List[LV]:
         return [t.variant for t in self.getAllLexemes()]
 
-def lineVariants(line: int, variants: List[LexemeVariant]):
+
+class FakeLexer(Lexer):
+    """For testing."""
+    def __init__(self, b: bytes):
+        super().__init__(io.BytesIO(b))
+
+
+def lineVariants(line: int, variants: List[LV]):
     return [(line, v) for v in variants]
 
-def testLexer():
-    lv = LexemeVariant
+
+def testLexer_basic():
+    expected = [(0, Lexeme(LV.NUMBER, b'42')), (0, EOF)]
+    result = FakeLexer(b'42').getAllLexemes()
+    assert expected == result
+
+
+def testLexer_helloWorld():
+    lv = LV
     ts = Lexer(open("grammar/hello_world.fn", 'rb', buffering=0))
     result = ts.getAllLexemes()
     resultVariants = [l[1].variant for l in result]
@@ -487,7 +521,6 @@ def testLexer():
         + [(6, lv.EOF)]
     )
 
-    pprint.pprint(result)
     assert lv.INVALID not in resultVariants
     assert expectLineVariants == resultLineVariants
 
@@ -499,7 +532,7 @@ def testLexer():
 # - Do lookahead of at least 1 lexeme, probably more (we will see)
 # - Advance the root (dropping nodes) up until a specific node.
 
-@dataclasses.dataclass
+@dataclass
 class LexemeNode:
     lexeme: Lexeme
     line: int
@@ -522,7 +555,7 @@ class LexemeLL(object):
         out = self.root
         self.root = out.next
         if self.root is None:
-            assert self.last == out
+            assert self.last is out
             self.last = None
         return out
 
@@ -566,49 +599,50 @@ class LexemeLL(object):
             self.lexer.scanner.line,
             self.lexer.scanner.col)
 
-        if self.end is None:
+        if self.last is None:
             assert self.root is None
             self.root = node
             self.last = node
         else:
             self.last.next = node
             self.last = node
-################################
-# AST Nodes
-
-class ASTNode(object):
-    pass
 
 
-@dataclasses.dataclass
-class Number(ASTNode):
-    value: int
-
-
-@dataclasses.dataclass
-class RawStr(ASTNode):
-    value: bytes
-
-
-@dataclasses.dataclass
-class Iden(ASTNode):
-    value: bytes
-
-
-@dataclasses.dataclass
-class Label(ASTNode):
-    value: bytes
-
+class FakeLexemeLL(LexemeLL):
+    """For testing."""
+    def __init__(self, b: bytes):
+        super().__init__(FakeLexer(b))
 
 ################################
-# Parser
+# AST Nodes and Parser
 # The parser implements the syntax defined in grammar/grammar.peg, which is
 # structured (ordered) such that it can be transparently implemented by a
 # recursive-descent style parser.
 #
 # The parser consumes the result of the lexer and converts it into AST nodes.
 
-@dataclasses.dataclass
+# ParsePtr
+# Mutable global parsers that will be defined later. Callers will use i.e.
+# parseStmt.ptr(ll) to call them.
+# 
+# Note: this is not really needed in python, it is mostly to demonstrate how
+# fngi/forth can implement it.
+
+@dataclass
+class ParsePtr:
+    ptr = None
+
+parseStmt = ParsePtr()
+parseExpr = ParsePtr()
+parseTy = ParsePtr()
+parseName = ParsePtr()
+parseMacro2 = ParsePtr()
+
+# Error handling is done through a global variable to demonstrate how to
+# implement on resource/syntax constrained languages like we will be doing in
+# Forth.
+
+@dataclass
 class ParseError:
     msg: str
     line: int
@@ -618,14 +652,71 @@ class ParseError:
     def new(cls, msg: any, ln: LexemeNode):
         return cls(str(msg), ln.line, ln.col)
 
-PARSE_ERROR: ParseError = None
+PARSE_ERROR = ParsePtr()
 
 def updateError(msg, ln: LexemeNode):
-    PARSE_ERROR = ParseError.new(msg, ln)
+    PARSE_ERROR.ptr = ParseError.new(msg, ln)
 
+def assertAndCleanError(expectedMsg = None):
+    if PARSE_ERROR.ptr:
+        result = PARSE_ERROR.ptr.msg
+    else:
+        result = None
+    PARSE_ERROR.ptr = None
+    assert expectedMsg == result
+
+
+def testError():
+    assert PARSE_ERROR.ptr is None
+    assertAndCleanError()
+    updateError('foo', LexemeNode(None, 11, 42))
+    assert PARSE_ERROR.ptr.msg == 'foo'
+    assert PARSE_ERROR.ptr.line == 11
+    assert PARSE_ERROR.ptr.col == 42
+    assertAndCleanError('foo')
+    assert PARSE_ERROR.ptr is None
+    assertAndCleanError()
+
+
+class ASTNode(object):
+    """The base AST node type."""
+    pass
+
+@dataclass
+class Number(ASTNode):
+    value: int
+
+@dataclass
+class PrimaryBytes(ASTNode):
+    variant: LV
+    value: bytes
+
+@dataclass
+class Void(ASTNode):
+    pass
+
+@dataclass
+class Empty(ASTNode):
+    pass
+
+@dataclass
+class LabelStmt(ASTNode):
+    label: bytes
+    stmt: ASTNode
+
+@dataclass
+class Block(ASTNode):
+    stmts: List[LabelStmt]
+
+################################
+# Parser
+# The parser is mostly a recursive descent parser. We implement the "least
+# complicated" expressions first and work our way up.
+
+# grammar: NUMBER = ("0x" _hex+) / _numeric+
 def parseNumber(ll: LexemeLL) -> Number:
     ln = ll.pop()
-    assert ln.lexeme.variant is LexemeVariant.NUMBER
+    assert ln.lexeme.variant is LV.NUMBER
     numTxt = ln.lexeme.text
     try:
         if len(numTxt) >= 2 and numTxt[1] == 'x':
@@ -637,32 +728,85 @@ def parseNumber(ll: LexemeLL) -> Number:
     return Number(value)
 
 
+def test_parseNumber():
+    ll = FakeLexemeLL(b'42')
+    result = parseNumber(ll)
+    assert 42 == result.value
+    result = ll.pop()
+    assert result.lexeme is EOF
+    assertAndCleanError()
+
+
 # TODO parseEscStr(ll: LexmeLL) -> EscStr:
-def parseRawStr(ll: LexmeLL) -> RawStr:
+def parseRawStr(ll: LexemeLL) -> PrimaryBytes:
     ln = ll.pop()
-    assert ln.lexeme.variant is LexemeVariant.RAW_STR
-    return RawStr(ln.lexeme.text[1:-1])
+    assert ln.lexeme.variant is LV.RAW_STR
+    return PrimaryBytes(LV.RAW_STR, ln.lexeme.text[1:-1])
 
 
-@dataclass.dataclass
-class ParsePtr:
-    ptr = None
-
-
-# Mutable globals that will be defined later. Callers will use i.e.
-# parseStmt.ptr(ll) to call them.
-parseStmt = ParsePtr()
-parseExpr = ParsePtr()
-parseTy = ParsePtr()
-parseName = ParsePtr()
-parseMacro2 = ParsePtr()
-
-
-def parseLabel(ll: LexemeLL) -> Label:
+# Grammar: label = w? "#" IDEN
+LABEL_ERR = "Expected IDEN after '#' for label"
+def parseLabel(ll: LexemeLL) -> PrimaryBytes:
     assert ll.pop().lexeme is LABEL
     ln = ll.pop()
 
-    if ln.lexeme.variant is not LexemeVariant.IDEN:
-        return updateError("Expected IDEN after '#' for label", ln)
+    if ln.lexeme.variant is not LV.IDEN:
+        return updateError(LABEL_ERR, ln)
 
-    return Label(ln.text)
+    return PrimaryBytes(LV.LABEL, ln.lexeme.text)
+
+def test_parseLabel():
+    ll = FakeLexemeLL(b'#foo')
+    result = parseLabel(ll)
+    assert b'foo' == result.value
+    assert ll.pop().lexeme is EOF
+
+def test_parseLabel_bad():
+    ll = FakeLexemeLL(b'#(foo)')
+    result = parseLabel(ll)
+    assert None == result
+    assertAndCleanError(LABEL_ERR)
+
+# Grammar: void = "[" w? "]"
+def parseVoid(ll: LexemeLL) -> Void:
+    assert ll.pop().lexeme is TYPE_OPEN
+    assert ll.pop().lexeme is TYPE_CLOSE
+    return PrimaryNoValue(LV.VOID)
+
+# Grammar: empty = "{" w? "}"
+def parseEmpty(ll: LexemeLL) -> Empty:
+    assert ll.pop().lexeme is DATA_OPEN
+    assert ll.pop().lexeme is DATA_CLOSE
+    return Empty()
+
+# Grammar: emptyBlock = "(" w? ")"
+def parseEmpty(ll: LexemeLL) -> Empty:
+    assert ll.pop().lexeme is BLOCK_OPEN
+    assert ll.pop().lexeme is BLOCK_CLOSE
+    return Empty()
+
+# Grammar: multiLabelStmt = label? stmt (SC label? stmt)* SC?
+# Note: we add on a bit here with endLexeme
+def parseMultiLabelStmt(ll: LexemeLL, endLexeme: Lexeme) -> (bool, List[LabelStmt]):
+    """Parse the label statements and consume the endLexeme."""
+    labelStmts = []
+    lastSemiColon = False
+    while True:
+        if ll.peek().lexeme is endLexeme:
+            ll.pop()
+            return lastSemiColon, labelStmts
+
+        label = None
+        if ll.peek().lexeme is LABEL:
+            label = parseLabel()
+            if label is None:
+                return None
+
+        stmt = parseStmt.ptr(ll)
+        if stmt is None:
+            return None
+
+        labelStmt = LabelStmt(label, stmt)
+        if ll.peek().lexeme is SEMICOLON:
+            ll.pop()
+            lastSemiColon = True
