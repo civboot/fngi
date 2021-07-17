26857b2efaade8b9c6bec7b8250b32f923fa4d3d
Rett Berg <vitiral@gmail.com>
2021-07-16 19:18:34 -0600

    simple run works.

--- fnpy/machine.py
+++ fnpy/machine.py
@@ -6,4 +6,30 @@
 # memory inside it. This allows us to define and execute functions.
 
 from .wasm_constants import *
+from .types import Env, ENV
 
+
+def run(env: Env, code):
+    ds = env.dataStack
+    for instr in code:
+        if type(instr) not in (tuple, list):
+            wi = instr
+        else:
+            wi = instr[0]
+        if wi == Wi32.const:
+            ds.push(I32(instr[1]))
+        elif wi == Wi32.add:
+            ds.push(I32(ds.pop(I32).value + ds.pop(I32).value))
+        elif wi == Wi32.mul:
+            ds.push(I32(ds.pop(I32).value * ds.pop(I32).value))
+
+def testRunSimple():
+    env = ENV.copyForTest()
+    run(env, [
+        (Wi32.const, 10),
+        (Wi32.const, 11),
+        Wi32.add,
+        (Wi32.const, 2),
+        Wi32.mul,
+    ])
+    assert 42 == env.dataStack.pop(I32).value
