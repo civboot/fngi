4652eb720565b76f36cfa9f611b5cfe3c6da8830
Rett Berg <rett@google.com>
2021-10-25 21:13:00 -0600

    start implementing parser

--- fu/fu.c
+++ fu/fu.c
@@ -2,6 +2,8 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <assert.h>
+#include <unistd.h>
+
 
 // ********************************************
 // ** Core Types
@@ -10,6 +12,9 @@ typedef uint8_t Bool;
 typedef uint8_t U8;
 typedef uint16_t U16;
 typedef uint32_t U32;
+typedef int8_t I8;
+typedef int16_t I16;
+typedef int32_t I32;
 typedef uint32_t ASz;
 typedef ASz APtr;
 
@@ -18,6 +23,8 @@ typedef CSz CPtr;
 
 #define ASIZE sizeof(ASz)
 #define CSIZE sizeof(CSz)
+#define FALSE 0
+#define TRUE 1
 
 // Size
 typedef enum {
@@ -49,6 +56,8 @@ typedef enum {
 #define MEM_SIZE 0x100000
 #define WS_SIZE  0x100
 #define RS_SIZE  0x100
+#define LS_SIZE  0x8000
+#define DICT_SIZE 0x1000
 
 // Generic stack.
 typedef struct {
@@ -62,13 +71,16 @@ typedef struct {
   U8* mem;
   APtr cp;            // seCtion Pointer
   APtr lp;            // local stack pointer
+  APtr heap;
 
   Stk ws;
   Stk rs;
+  Stk ls;
+  Stk dict;
 } Env;
 
 typedef struct {
-  U32 v[3];
+  U32 v[3]; // value "stack". 0=top, 1=scnd, 2=extra
   SzBits sz;
   U8 len;
   Bool usesImm;
@@ -85,9 +97,9 @@ void fail(U8* cstr) {
 
 U8 szBits_toBytes(SzBits sz) {
   switch (sz) {
-    case S_U8: 1; break;
-    case S_U16: 2; break;
-    case S_U32: 4; break;
+    case S_U8: return 1;
+    case S_U16: return 2;
+    case S_U32: return 4;
     default: fail("invalid Sz");
   }
 }
@@ -139,64 +151,150 @@ U32 Stk_pop(Stk* stk, SzBits sz) {
   return out;
 }
 
+// Shift opdata to the right.
+void shift_op(OpData* out) {
+  out->v[2] = out->v[1];
+  out->v[1] = out->v[0];
+  out->len += 1;
+}
+
 // ********************************************
 // ** Operations
 
 #define OP_ARGS Env* env, OpData *out
-typedef void (*op_t)(OP_ARGS);
+#define OP_ASSERT(COND, MSG) \
+  if(!(COND)) { printf(MSG); printf("\n"); return 1; }
 
-void op_notimpl(OP_ARGS) {
+#define OP_CHECK(COND) \
+  if(COND) { return 1; }
+
+typedef U8 (*op_t)(OP_ARGS);
+
+U8 op_notimpl(OP_ARGS) {
   fail("op not implemented");
 }
 
-void op_fetch(OP_ARGS) {
-  out->v[0] = fetch(env->mem, out->v[0], out->sz);
+U8 op_fetch(OP_ARGS) { out->v[0] = fetch(env->mem, out->v[0], out->sz); }
+U8 op_store(OP_ARGS) { store(env->mem, out->v[1], out->v[0], out->sz); out->len = 0; }
+// DVF
+// DVS
+U8 op_nop(OP_ARGS) {};
+U8 op_drp(OP_ARGS) { out->v[0] = out->v[1]; out->len -= 1; };
+U8 op_inv(OP_ARGS) { out->v[0] = ~out->v[0]; };
+U8 op_neg(OP_ARGS) {
+  switch (out->sz) {
+    case S_U8: out->v[0] = (U32) (-(I8)out->v[0]); break;
+    case S_U16: out->v[0] = (U32) (-(I16)out->v[0]); break;
+    case S_U32: out->v[0] = (U32) (-(I32)out->v[0]); break;
+  }
 }
-
-void op_store(OP_ARGS) {
-  store(env->mem, out->v[1], out->v[0], out->sz);
-  out->len = 0;
-}
-
-void op_nop(OP_ARGS) {};
-
+U8 op_eqz(OP_ARGS) { out->v[0] = out->v[0] == 0; }
+U8 op_eqz_nc(OP_ARGS) { shift_op(out); out->v[0] = out->v[1] == 0; }
+U8 op_drop2(OP_ARGS) { out->len = 0; }
+U8 op_ovr(OP_ARGS) { shift_op(out); out->v[0] = out->v[2]; }
+U8 op_add(OP_ARGS) { out->v[0] = out->v[1] + out->v[0]; out->len = 1; }
+U8 op_sub(OP_ARGS) { out->v[0] = out->v[1] - out->v[0]; out->len = 1; }
+U8 op_mod(OP_ARGS) { out->v[0] = out->v[1] % out->v[0]; out->len = 1; }
+U8 op_mul(OP_ARGS) { out->v[0] = out->v[1] * out->v[0]; out->len = 1; }
 
 op_t ops[] = {
   // FT,          SR,           DVF,          DVS,
   op_fetch,       op_store,     op_notimpl,   op_notimpl,
 
   // NOP,         DRP,          INV,          NEG,
-  op_nop,         op_notimpl,   op_notimpl,   op_notimpl,
+  op_nop,         op_drp,       op_inv,       op_neg,
 
-  // EQZ,        EQZ_NC,       DRP2,         OVR,
-  op_notimpl,    op_notimpl,   op_notimpl,   op_notimpl,
+  // EQZ,        EQZ_NC,        DRP2,         OVR,
+  op_eqz,        op_eqz_nc,     op_drop2,     op_ovr,
 
-  // ADD,          SUB,          MOD,
-  op_notimpl,    op_notimpl,   op_notimpl,   op_notimpl,
+  // ADD,        SUB,           MOD,          MUL,
+  op_add,        op_notimpl,    op_notimpl,   op_notimpl,
 };
 
 // ********************************************
-// ** Initialization and Main
-#define NEW_ENV(WSM, RSM, MEM)            \
-  {                                       \
-    .mem = MEM,                           \
+// ** Parser
+
+#define PARSER_BUF_LEN 32
+typedef struct {
+  U8* src;
+  U32 i;
+  U32 srcLen;
+  U8 b[PARSER_BUF_LEN];
+  U8 bLen;
+} Parser;
+
+#define IS_WHITESPC(C) (C<=' ')
+
+void parser_skipWhitespace(Parser *p) {
+  while(p->i < p->srcLen) {
+    U8 c = p->src[p->i];
+    if(!IS_WHITESPC(c)) {
+      return;
+    }
+    p->i += 1;
+  }
+}
+
+U8 parser_insert(Parser* p, U8 c) {
+    OP_ASSERT(p->bLen < PARSER_BUF_LEN, "Parser buf overflow");
+    p->b[p->bLen] = c;
+    p->bLen += 1;
+    p->i += 1;
+}
+
+U8 parser_fillBuffer(Parser* p) {
+  while(p->i < p->srcLen) {
+    U8 c = p->src[p->i];
+    if(IS_WHITESPC(c)) { return 0; }
+    OP_CHECK(parser_insert(p, c));
+  }
+}
+
+U8 parser_nextComma(Parser* p) {
+  while(p->i < p->srcLen) {
+    U8 c = p->src[p->i];
+    OP_ASSERT(!IS_WHITESPC(c), "whitespace in instr");
+    OP_CHECK(parser_insert(p, c));
+  }
+}
+
+void fuParse(Env* env, Parser* p) {
+}
+
+// ********************************************
+// ** Initialization
+#define NEW_ENV                           \
+  U8 mem[MEM_SIZE];                       \
+  U8 wsMem[WS_SIZE];                      \
+  U8 rsMem[RS_SIZE];                      \
+  Env env = {                             \
+    .mem = mem,                           \
     .cp = 0,                              \
-    .lp = MEM_SIZE,                       \
-    .ws = { .sp = WS_SIZE, .mem = WSM },  \
-    .rs = { .sp = RS_SIZE, .mem = RSM },  \
+    .heap = 0,                            \
+    .ws = { .sp = WS_SIZE, .mem = wsMem },\
+    .rs = { .sp = RS_SIZE, .mem = rsMem },\
+    .ls = {                               \
+      .sp = LS_SIZE,                      \
+        .mem = mem+MEM_SIZE-LS_SIZE       \
+    },                                    \
+    .dict = {                             \
+      .sp = DICT_SIZE,                    \
+      .mem = mem+MEM_SIZE-LS_SIZE-DICT_SIZE, \
+    },                                    \
   }
 
 
+// ********************************************
+// ** Tests
+
+
+void tests() {
+}
+
 int main() {
-  U8 mem[MEM_SIZE];
-  U8 wsMem[WS_SIZE];
-  U8 rsMem[RS_SIZE];
+  printf("compiling fu...:\n");
 
-  printf("Fu\n");
-  printf("%i", SRCP);
 
-  Env env = NEW_ENV(mem, wsMem, rsMem);
-
-  fail("bad");
+  tests();
   return 0;
 }
